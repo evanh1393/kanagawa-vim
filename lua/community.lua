ae5d8f4ddc330527bd4fc5ba78983dc4597d4abd
evanh1393 <evanh1393@gmail.com>
2025-05-03 21:14:26 +0000

    pushing

--- lua/community.lua
+++ lua/community.lua
@@ -5,7 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" }
+  -- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   -- import/override with your plugins folder
 }
